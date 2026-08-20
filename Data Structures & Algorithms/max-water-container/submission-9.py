b3da6f3d45717ef94db8bf1007f80eaad89adcbc
Ayub Abdelgawad <ayubabdelgawad@gmail.com>
2026-08-20 17:17:15 -0400

    Add: max-water-container - submission-9

---
+++ Data Structures & Algorithms/max-water-container/submission-9.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        biggest = 0
+        i, j = 0, len(heights) - 1
+
+        while i < j:
+            area = (j - i) * min(heights[i], heights[j])
+            biggest = max(biggest, area)
+            if heights[i] < heights[j]:
+                i += 1
+            elif heights[i] > heights[j]:
+                j -= 1
+            else:
+                i += 1
+
+            if i == j:
+                break
+        return biggest
+
+
+        #i at start iterate till end
+        #j at end iterate at start
+        # stop both when they meet
+        # keep comparing final output of both until biggest appears, until i and j meet
